ec0ba910348b4263392663a6abbe441127e749be
zumhagen <benjamin.zumhagen@here.com>
2017-11-21 19:27:45 -0600

    Finishing example
    Change-Id: I8bef327215f4239e83c7dc15ad24a2d6144f2c20

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+let Game = require('../models/game');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let games = Game.find({});
+  games.exec((err, games) => {
+  	res.render('index', { title: 'Express2', games: games });
+  });
 });
 
 module.exports = router;
